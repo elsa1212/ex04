af9cb48b884ea4559dd4921036ea78c5ee232400
Gil Jae Won <49782452+elsa1212@users.noreply.github.com>
2023-07-31 16:44:13 +0900

    HomeController Print로 변경

--- src/main/java/com/kh/ex04/HomeController.java
+++ src/main/java/com/kh/ex04/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		System.out.println("Home Controller");
+		System.out.println("HomeController Print");
 		return "home";
 	}
 	
